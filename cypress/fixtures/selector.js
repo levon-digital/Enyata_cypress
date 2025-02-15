9b959432853bbac68322677a9bf075e02439b3f7
Levon-Digital <adewaleogunkoyodey200@gmail.com>
2025-02-15 21:36:24 +0100

    Assessment_UI

---
+++ cypress/fixtures/selector.js
@@ -0,0 +1,68 @@
+export const Login = {
+  "usernamefield" : '[data-test="username"]',
+  "passwordfield" : '[data-test="password"]',
+  "loginbutton" : '[data-test="login-button"]',
+  "ValidUsername" : "standard_user",
+   "ValidPassword": "secret_sauce",
+   "PageTitle": ".title",
+
+
+}
+
+export const Add_to_cart = {
+ "Itemname1": '[data-test="item-4-title-link"] > [data-test="inventory-item-name"]',
+ "ItemAmount1": ':nth-child(1) > [data-test="inventory-item-description"] > .pricebar > [data-test="inventory-item-price"]',
+ "Add_to_cart_button1": '[data-test="add-to-cart-sauce-labs-backpack"]',
+ "Itemname2": '[data-test="item-0-title-link"] > [data-test="inventory-item-name"]',
+ "ItemAmount2": ':nth-child(2) > [data-test="inventory-item-description"] > .pricebar > [data-test="inventory-item-price"]',
+ "Add_to_cart_button2": '[data-test="add-to-cart-sauce-labs-bike-light"]',
+"Cart_button": '[data-test="shopping-cart-link"]',
+"Cart_page_title": '[data-test="title"]',
+"Amount_cartpage1": ':nth-child(3) > .cart_item_label > .item_pricebar > [data-test="inventory-item-price"]',
+"Amount_cartpage2": ':nth-child(4) > .cart_item_label > .item_pricebar > [data-test="inventory-item-price"]',
+"checkout_informationpage": '[data-test="title"]',
+ "checkout_firstname": '[data-test="firstName"]',
+ "checkout_lastname": '[data-test="lastName"]',
+ "checkout_postalcode": '[data-test="postalCode"]',
+ "checkout_continue": '[data-test="continue"]',
+ "checkout_overviewPAGE": '[data-test="title"]',
+ "QTY1": ':nth-child(3) > [data-test="item-quantity"]',
+ "QTY2": ':nth-child(4) > [data-test="item-quantity"]',
+"Taxamount": '[data-test="tax-label"]',
+"Totalamount": '[data-test="tax-label"]',
+"checkout_finish": '[data-test="finish"]',
+"checkout_complete": '[data-test="title"]',
+
+}
+
+export const Purchase = {
+ "Itemname1": '[data-test="item-4-title-link"] > [data-test="inventory-item-name"]',
+ "ItemAmount1": ':nth-child(1) > [data-test="inventory-item-description"] > .pricebar > [data-test="inventory-item-price"]',
+ "Add_to_cart_button1": '[data-test="add-to-cart-sauce-labs-backpack"]',
+ "Itemname2": '[data-test="item-0-title-link"] > [data-test="inventory-item-name"]',
+ "ItemAmount2": ':nth-child(2) > [data-test="inventory-item-description"] > .pricebar > [data-test="inventory-item-price"]',
+ "Add_to_cart_button2": '[data-test="add-to-cart-sauce-labs-bike-light"]',
+"Cart_button": '[data-test="shopping-cart-link"]',
+"Cart_page_title": '[data-test="title"]',
+"Amount_cartpage1": ':nth-child(3) > .cart_item_label > .item_pricebar > [data-test="inventory-item-price"]',
+"Amount_cartpage2": ':nth-child(4) > .cart_item_label > .item_pricebar > [data-test="inventory-item-price"]',
+"checkout_informationpage": '[data-test="title"]',
+ "checkout_firstname": '[data-test="firstName"]',
+ "checkout_lastname": '[data-test="lastName"]',
+ "checkout_postalcode": '[data-test="postalCode"]',
+ "checkout_continue": '[data-test="continue"]',
+ "checkout_overviewPAGE": '[data-test="title"]',
+ "QTY1": ':nth-child(3) > [data-test="item-quantity"]',
+ "QTY2": ':nth-child(4) > [data-test="item-quantity"]',
+"Taxamount": '[data-test="tax-label"]',
+"Totalamount": '[data-test="tax-label"]',
+"checkout_finish": '[data-test="finish"]',
+"checkout_complete": '[data-test="title"]',
+
+}
+
+export const filter = {
+ "filterButton": '.select_container',
+
+
+}
